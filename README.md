b52aa5b078073bddad923b114b173a220b3969e3
Anton <anton@adc.sh>
2020-02-22 20:15:01 +0000

    password example

--- README.md
+++ README.md
@@ -190,7 +190,7 @@ const Password = async () => {
 }
 ```
 ```
-
+Please enter the password: ********
 ```
 
 <details>
--- example/password/index.js
+++ example/password/index.js
@@ -1,10 +1,9 @@
 import { fork } from 'spawncommand'
 import forkFeed from 'forkfeed'
 
-const { stdout, stdin } = fork('node_modules/.bin/alanode', ['example/password/fork'], {
+const { stdout, stdin } = fork('node_modules/alamode/build/alanode.js', ['example/password/fork'], {
   stdio: 'pipe',
 })
-
 forkFeed(stdout, stdin, [
   [/password/, '********'],
 ], process.stdout)
